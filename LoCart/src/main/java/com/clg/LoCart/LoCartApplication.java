0d0271395caace08829207813e0c6ae3735057ce
Tanuj Keshattiwar <tanujkeshattiwar@gmail.com>
2025-03-31 14:50:37 +0530

    Created Project File Structure

---
+++ LoCart/src/main/java/com/clg/LoCart/LoCartApplication.java
@@ -0,0 +1,13 @@
+package com.clg.LoCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoCartApplication.class, args);
+	}
+
+}
---
+++ LoCart/src/test/java/com/clg/LoCart/LoCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clg.LoCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
